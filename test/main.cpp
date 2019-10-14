cc860907ccba49dd491d09713dcad94a420c1fa0
Lars Melchior <TheLartians@users.noreply.github.com>
2019-10-14 14:18:31 +0200

    init

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#define a "xxxxxxxxxxx"
+#define b a a a a a a a
+#define c b b b b b b b
+#define d c c c c c c c
+#define e d d d d d d d
+#define f e e e e e e e
+#define g f f f f f f f
+#define h g g g g g g g
+#define i h h h h h h h
+#define j i i i i i i i
+
+int main(){
+  const char * z = j;
+  return 0;
+}
